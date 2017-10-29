fd28bb342fc542c493b98788c299d84e922f2425
khadijakhaldi <khadijaakhaldi@gmail.com>
2017-10-29 21:42:41 +0000

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,1 +1,16 @@
 
+##Plot2
+path<-"Houshold Project"
+
+activities <- read.table(file.path(path, "household_power_consumption.txt"))
+power<- read.table(file.path(path, "household_power_consumption.txt"))
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1",
+"Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+#When i use strptime one the Time only i get wrong expression
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
